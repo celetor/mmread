5a1d4d03beddccceb476662944b00272c37f347a
celetor <koncknock@yandex.com>
2022-01-12 13:21:03 +0800

    ver-1.6.0

--- build.bat
+++ build.bat
@@ -7,8 +7,8 @@ python -m nuitka --standalone --mingw64 ^
 --include-qt-plugins=sensible,styles ^
 --windows-company-name="Github Celeter" ^
 --windows-product-name=mmread-download ^
---windows-file-version=1.5.0.0 ^
---windows-product-version=1.5.0.0 ^
+--windows-file-version=1.6.0.0 ^
+--windows-product-version=1.6.0.0 ^
 --windows-file-description="微信读书下载工具" ^
 --output-dir=output ^
 --windows-disable-console ^
